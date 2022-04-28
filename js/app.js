b561b3677710b8e2edb4e9e2e10def0ccfcd70e1
Johan Meneses <johanmeci03@gmail.com>
2022-04-28 14:39:25 -0500

    Adding swiper carousel and image API

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const iconsAction = document.querySelectorAll('.icon');
+
+const swiperWrapper = document.querySelector('#swiper-wrapper');
+
 const divComments = document.querySelector('#commentsCard');
 const inputComment = document.querySelector('#comment');
 const btnPostComment = document.querySelector('#btnPost');
@@ -23,30 +26,78 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 
-    //Post comment action
-    btnPostComment.addEventListener('click', () => {
-
-        if (inputComment.value !== '') {
-
-            //Create HTML comment
-            let divComment = document.createElement('div');
-            divComment.classList.add('comment');
-
-            let txtComment = document.createElement('p');
-            txtComment.innerHTML = `<span class='userName'>comment</span> ${inputComment.value}`;
-            
-            //Add comment text in div
-            divComment.appendChild(txtComment);
-
-            //Add comment to comments container
-            divComments.appendChild(divComment);
-
-            //Clean input comment
-            inputComment.value = '';
-
-            return;
-
-        }
-    });
+    //Post comment listener
+    btnPostComment.addEventListener('click', postComment);
 
 });
+
+//Post Comment Function
+const postComment = () => {
+
+    if (inputComment.value !== '') {
+
+        //Create HTML comment
+        const divComment = document.createElement('div');
+        divComment.classList.add('comment');
+
+        const txtComment = document.createElement('p');
+        txtComment.innerHTML = `<span class='userName'>comment</span> ${inputComment.value}`;
+        
+        //Add comment text in div
+        divComment.appendChild(txtComment);
+
+        //Add comment to comments container
+        divComments.appendChild(divComment);
+
+        //Clean input comment
+        inputComment.value = '';
+
+        return;
+
+    }
+
+}
+
+//Show images HTML
+const showImgApi = (imgData) => {
+
+    imgData.map(img => {
+        // console.log(img.download_url);
+        const swiperSlide = document.createElement('div');
+        swiperSlide.classList.add('swiper-slide');
+
+        const slideImg = document.createElement('img');
+        slideImg.src = img.download_url;
+
+        swiperSlide.appendChild(slideImg);
+
+        swiperWrapper.appendChild(swiperSlide);
+    });
+}
+
+
+const urlAPI = 'https://picsum.photos/v2/list?page=1&limit=5';
+
+fetch(urlAPI)
+    .then( response => response.json() )
+    .then( imgData => {
+
+        while (swiperWrapper.firstChild) {
+            swiperWrapper.removeChild(swiperWrapper.firstChild);
+        }
+
+        showImgApi(imgData);
+
+        /* Images Card */
+        const swiper = new Swiper('.swiper', {
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            }
+        });
+
+    })
+    .catch( error => console.log('Error API request:', error.message) );
